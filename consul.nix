5d9d27551a6f03a6564666b71cbf92fc8122a69b
TJ Horner <me@tjhorner.com>
2021-12-10 20:44:15 -0500

    🧐🧐🧐🧐

--- consul.nix
+++ consul.nix
@@ -9,7 +9,7 @@
       bootstrap = true;
       bootstrap_expect = 1;
 
-      # bind_addr = "{{ GetAllInterfaces | include \"name\" \"^eth\" | include \"flags\" \"forwardable|up\" | attr \"address\" }}";
+      bind_addr = "{{ GetAllInterfaces | include \"name\" \"^eth\" | include \"flags\" \"forwardable|up\" | attr \"address\" }}";
       # advertise_addr_wan = "{{ GetAllInterfaces | include \"name\" \"^tailscale\" | include \"flags\" \"forwardable|up\" | attr \"address\" }}";
     };
   };

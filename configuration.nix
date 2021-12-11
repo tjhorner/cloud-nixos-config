33d086283d9734f36304b8b8599a88c0dde0ea85
TJ Horner <me@tjhorner.com>
2021-12-10 19:41:42 -0500

    modify config

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,11 @@
   imports = [
     <nixpkgs/nixos/modules/virtualisation/google-compute-image.nix>
     ./nomad.nix
+    ./update-config.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    git
   ];
 
   system.autoUpgrade.enable = true;

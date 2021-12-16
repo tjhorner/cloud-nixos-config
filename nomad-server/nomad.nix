8201c453df12dceb20901ffbb37086cfae8afb16
TJ Horner <me@tjhorner.com>
2021-12-15 23:53:32 -0500

    oh boy

--- nomad-server/nomad.nix
+++ nomad-server/nomad.nix
@@ -2,9 +2,22 @@
 {
   virtualisation.docker.enable = true;
 
+  systemd.services.nomad-dynamic-config = {
+    enable = true;
+    before = [ "nomad.service" ];
+    serviceConfig.Type = "oneshot";
+
+    script = ''
+      BOOTSTRAP_EXPECT=$(${pkgs.curl}/bin/curl "http://metadata.google.internal/computeMetadata/v1/instance/attributes/bootstrap-expect" -H "Metadata-Flavor: Google")
+      mkdir -p /etc/nomad.d
+      echo "server { bootstrap_expect = $BOOTSTRAP_EXPECT }" > /etc/nomad.d/bootstrap_expect.hcl
+    '';
+  };
+
   services.nomad = {
     enable = true;
     enableDocker = true;
+    extraSettingsPaths = [ "/etc/nomad.d" ];
 
     settings = {
       datacenter = "gcp";

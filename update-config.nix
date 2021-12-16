9f30c143a03a2966ce7fd1430c409ce649d1c019
TJ Horner <me@tjhorner.com>
2021-12-15 20:04:53 -0500

    systemd timers are cooler

--- update-config.nix
+++ update-config.nix
@@ -1,9 +1,21 @@
 { config, pkgs, ... }:
 {
-  services.cron = {
-    enable = true;
-    systemCronJobs = [
-      "*/5 * * * *      root    cd /etc/nixos && git pull origin master && nixos-rebuild switch"
-    ];
+  systemd.services.update-nixos-config = {
+    serviceConfig.Type = "oneshot";
+    path = with pkgs; [ git bash nix nixos-rebuild ];
+    script = ''
+      cd /etc/nixos
+      git pull
+      nixos-rebuild switch
+    '';
+  };
+
+  systemd.timers.update-nixos-config = {
+    wantedBy = [ "timers.target" ];
+    partOf = [ "update-nixos-config.service" ];
+    timerConfig = {
+      OnCalendar = "*:0/5"; # every 5 mins
+      Unit = "update-nixos-config.service";
+    };
   };
 }

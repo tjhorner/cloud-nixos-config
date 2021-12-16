100e72bf8cdbcde6329c1e4fbd4bc64be316f89e
TJ Horner <me@tjhorner.com>
2021-12-16 00:13:48 -0500

    all new

--- consul-server/consul.nix
+++ consul-server/consul.nix
@@ -1,5 +1,18 @@
 { config, pkgs, ... }:
 {
+  systemd.services.consul-dynamic-config = {
+    enable = true;
+    before = [ "consul.service" ];
+    wantedBy = [ "consul.service" ];
+    serviceConfig.Type = "oneshot";
+
+    script = ''
+      BOOTSTRAP_EXPECT=$(${pkgs.curl}/bin/curl "http://metadata.google.internal/computeMetadata/v1/instance/attributes/bootstrap-expect" -H "Metadata-Flavor: Google")
+      mkdir -p /etc/consul.d
+      echo "bootstrap_expect = $BOOTSTRAP_EXPECT" > /etc/consul.d/bootstrap_expect.hcl
+    '';
+  };
+
   services.consul = {
     enable = true;
     webUi = true;
@@ -9,12 +22,6 @@
       datacenter = "gcp";
       retry_join = [ "provider=gce tag_value=consul-server" ];
       bind_addr = "{{ GetAllInterfaces | include \"name\" \"^eth\" | include \"flags\" \"forwardable|up\" | include \"type\" \"ipv4\" | attr \"address\" }}";
-
-      # addresses = {
-      #   http = "{{ GetAllInterfaces | include \"name\" \"^tailscale\" | include \"flags\" \"forwardable|up\" | attr \"address\" }}";
-      # };
-
-      # advertise_addr_wan = "{{ GetAllInterfaces | include \"name\" \"^tailscale\" | include \"flags\" \"forwardable|up\" | attr \"address\" }}";
     };
   };
 }

3a6f26870e83485595ba8576d3d3d849cb37d0ae
TJ Horner <me@tjhorner.com>
2021-12-16 12:30:45 -0500

    idk

--- nomad-server/nomad.nix
+++ nomad-server/nomad.nix
@@ -63,6 +63,18 @@
 
       client = {
         enabled = true;
+
+        chroot_env = {
+          "/bin" = "/bin";
+          "/etc" = "/etc";
+          "/lib" = "/lib";
+          "/lib32" = "/lib32";
+          "/lib64" = "/lib64";
+          "/run/resolvconf" = "/run/resolvconf";
+          "/sbin" = "/sbin";
+          "/usr" = "/usr";
+          "/nix" = "/nix";
+        };
       };
     };
   };
